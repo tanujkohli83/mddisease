42f5ac43545759ef9a24346f84f88119e7a376ea
Tanuj kohli <113450384+tanujkohli83@users.noreply.github.com>
2025-05-11 19:01:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -231,6 +231,6 @@ hide_st_style = """
             footer {visibility: hidden;}
             header {visibility: hidden;}
             </style>
-              Design and Developed by Japanjot Singh 
+              Design and Developed by Kirpa Bajaj and Yatharth Chauahan
             """
 st.markdown(hide_st_style , unsafe_allow_html=True)
